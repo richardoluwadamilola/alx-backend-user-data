251f6a76b22a34c172cfd6f6a487eca5752ba8e2
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-05-29 07:52:07 +0000

    error handling

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,11 +2,11 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 from api.v1.views import app_views
